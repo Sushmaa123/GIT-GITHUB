bf3ea04468d42fc37f8cb8c6d5f26c1d4e226b19
Gouthukutty <151035654+Gouthukutty@users.noreply.github.com>
2026-06-09 15:26:41 +0530

    Update test2.java

--- test2.java
+++ test2.java
@@ -2,6 +2,7 @@ public class test2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("this is from test2 ");
+ System.out.println("this is edited from pull ");        
 
     }
 }
