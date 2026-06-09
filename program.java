f76d4a45c62c23899ad348803b4afc1f4b3d56c8
Arpitha <arpithauk7@gmail.com>
2026-06-09 15:12:41 +0530

    Modified the program.java from arpitha branch

--- program.java
+++ program.java
@@ -2,5 +2,6 @@ class program {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Java Programming");
+        System.out.println("Java Programming Example");
     }
 }
